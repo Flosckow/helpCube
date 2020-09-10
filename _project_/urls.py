365e82e3469f8e7a04b73923d3d40949196eef95
MaryaskinAV <maryaskin.a.v@gmail.com>
2020-09-11 02:12:24 +0500

    Правка настроек проекта

--- _project_/urls.py
+++ _project_/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from users.views import UserLogin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('social_django.urls', namespace='social')),  # TODO нужно ли этоо здесь?
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+    path('login/', UserLogin.as_view(), name='login'),
 
+
+    # base admin
+    path('admin/', admin.site.urls),
 ]
