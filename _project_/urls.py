9f4372589bec959630b2866058882912f30b2884
MaryaskinAV <maryaskin.a.v@gmail.com>
2020-09-11 02:16:09 +0500

    Adding urls static files

--- _project_/urls.py
+++ _project_/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -8,7 +9,13 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('login/', UserLogin.as_view(), name='login'),
 
-
     # base admin
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
