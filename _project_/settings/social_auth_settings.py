43a92183712fa1cec67e125365fd494a4768ebff
Sergey(lion_heard) <korsakovsergey07062000@gmail.com>
2020-09-17 02:16:40 +0300

    Вход через Facebook

--- _project_/settings/social_auth_settings.py
+++ _project_/settings/social_auth_settings.py
@@ -22,3 +22,14 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+# Facebook settings
+SOCIAL_FACEBOOK_ID = "328787081719892"
+SOCIAL_FACEBOOK_TOKEN = "9f907bbab9aa1fd3e260cef41df00c68"
+SOCIAL_FACEBOOK_API_URL = 'https://graph.facebook.com/me/?'
+SOCIAL_FACEBOOK_AUTH_URL = 'https://www.facebook.com/v8.0/dialog/oauth?'
+SOCIAL_FACEBOOK_TOKEN_URL = 'https://graph.facebook.com/v8.0/oauth/access_token?'
+SOCIAL_FACEBOOK_REDIRECT_URL = "http://localhost:8000/social/fb"
+
+SOCIAL_FACEBOOK_DEBUG_TOKEN_URL = 'https://graph.facebook.com/debug_token?'  # TODO пока не используется
+SOCIAL_FACEBOOK_ACCESS_TOKEN = "328787081719892|JQlxsenjuByFjPLgS03ZDfwDaVI"  # TODO пока не используется
