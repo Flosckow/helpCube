e229a3b714f0e39e9df61bc50d96746ff65e19e6
Sergey(lion_heard) <korsakovsergey07062000@gmail.com>
2020-09-17 02:21:19 +0300

    Удалил django social

--- _project_/settings/social_auth_settings.py
+++ _project_/settings/social_auth_settings.py
@@ -1,28 +1,5 @@
 # ---Django_social---
 
-SOCIAL_AUTH_POSTGRES_JSONFIELD = True
-
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.github.GithubOAuth2',  # TODO добавить GitLab
-    'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.google.GooglePlusAuth',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-# TODO  SOCIAL_AUTH_URL_NAMESPACE = 'social'
-
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',  # TODO не очень надёжная штука судя по докам
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
-
 # Facebook settings
 SOCIAL_FACEBOOK_ID = "328787081719892"
 SOCIAL_FACEBOOK_TOKEN = "9f907bbab9aa1fd3e260cef41df00c68"
