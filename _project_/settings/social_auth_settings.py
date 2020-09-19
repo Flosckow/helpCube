7a15b323cae29dc2bbb034c9e8159e9492a8bb01
Sergey(lion_heard) <korsakovsergey07062000@gmail.com>
2020-09-19 23:54:24 +0300

    Настройки и url для Github

--- _project_/settings/social_auth_settings.py
+++ _project_/settings/social_auth_settings.py
@@ -10,3 +10,13 @@ SOCIAL_FACEBOOK_REDIRECT_URL = "http://localhost:8000/social/fb"
 
 SOCIAL_FACEBOOK_DEBUG_TOKEN_URL = 'https://graph.facebook.com/debug_token?'  # TODO пока не используется
 SOCIAL_FACEBOOK_ACCESS_TOKEN = "328787081719892|JQlxsenjuByFjPLgS03ZDfwDaVI"  # TODO пока не используется
+
+# Github settings
+SOCIAL_GITHUB_ID = '7642118bb47047141802'
+SOCIAL_GITHUB_SECRET = 'd96fffe82d8f2fcb006409c73d2ea8a086092674'
+SOCIAL_GITHUB_REDIRECT_URL = 'http://localhost:8000/social/github'
+SOCIAL_GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize?'
+SOCIAL_GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token?"
+SOCIAL_GITHUB_GET_USER_DATA = "https://api.github.com/user?"
+SOCIAL_GITHUB_GET_USER_EMAIL = "https://api.github.com/user/emails?"
+
